9bb59ffe354dbdb3a7cf9b31d3ce54bcc3fb665c
钱良虎 <1751660300@qq.com>
2020-10-09 23:20:55 +0800

    qlh 注册功能

--- js/login.js
+++ js/login.js
@@ -59,7 +59,7 @@ $("button.c").click(function(){
 		type:"POST",
 		data:datas,
 		success:function(res){
-			console.log(res)
+			// console.log(res)
 			for(var i in res){
 				// console.log(res[i].uid, res[i].upw)
 				if(res[i].uid == datas["card"] && res[i].upw == datas["pass"]){
@@ -113,9 +113,17 @@ function myxr(userinfo){
 	var use = userinfo[0]
 	// background-image: url("https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1601616870285&di=ff31dc3a8dac28d6693afa1b39707c24&imgtype=0&src=http%3A%2F%2Fb-ssl.duitang.com%2Fuploads%2Fitem%2F201901%2F19%2F20190119231949_buetu.jpg");
 	console.log(use)
+	if(use.uheadimg.trim() == ""){
+		$(".userinfo .left .headimg").html("上传头像")
+	}else{
+		$(".userinfo .left .headimg").css({"background-image":"url(\""+use.uheadimg+"\")"})
+	}
+	if(use["uname"] == ""){
+		$(".userinfo .left .uname h2").html("双击修改姓名")
+	}else{
+		$(".userinfo .left .uname h2").html(use["uname"])
+	}
 	
-	$(".userinfo .left .headimg").css({"background-image":"url(\""+use.uheadimg+"\")"})
-	$(".userinfo .left .uname h2").html(use["uname"])
 	$(".nr").each(function(){
 		$(this).children("span").html(use[$(this).prop("id")])
 	})
@@ -156,4 +164,25 @@ $(".foot div:nth-last-child(3)").click(function(){
 })
 
 
-
+// 注册的功能
+$(".zpt #check").click(function(){
+	var uid = $(".zpt #zcard").prop("value")
+	var pwd = $(".zpt #zpwd").prop("value")
+	var zqpwd = $(".zpt #zqpwd").prop("value")
+	if(zqpwd != pwd){
+		alert("两次输入的密码不相同")
+	}else{
+		$.ajax({
+			url:"http://127.0.0.1:5000/zc",
+			type:"post",
+			data:{"uid": uid, "pwd": pwd},
+			success:function(result){
+				if(result.type == true){
+					alert("注册成功")
+				}else{
+					alert("注册失败")
+				}
+			}
+		})
+	}
+})
